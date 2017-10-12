0770b26eafcc6bc3387e6649c9660461de939a3c
Thor Andre Andersen <thorandre.andersen@gmail.com>
2017-10-12 22:23:43 +0200

    Enabled migrations for Dal

---
+++ FamilyResearch.Data/Migrations/201710122008023_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace FamilyResearch.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Surname = c.String(),
+                        FirstName = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
